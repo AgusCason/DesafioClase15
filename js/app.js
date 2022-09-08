d76badb716b478c2cf3c4f4d5f64f099f35d1cd9
AgusCason <88736454+AgusCason@users.noreply.github.com>
2022-09-08 18:05:11 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,23 +1,79 @@
-const pelis = document.querySelector("#pelis") ;
-const URL = "js/peliculas.json"//"https://jsonplaceholder.typicode.com/posts" 
-  let tf = [] //Array vacío, nos permite cargar la respuesta de FETCH, nuestro "Servidor Remoto"
+const pelisDrama = document.querySelector("#pelisDrama") ;
+const pelisComedia = document.querySelector("#pelisComedia") ;
+const pelisTendencia = document.querySelector("#pelisTendencia") ;
+const pelisRecomendaciones = document.querySelector("#pelisRecomendaciones") ;
+
+const Drama = "https://aguscason.github.io/DesafioClase15/js/drama.json" 
+const Comedia = "https://aguscason.github.io/DesafioClase15/js/comedia.json" 
+const Recomendado = "https://aguscason.github.io/DesafioClase15/js/recomendado.json" 
+const Tendencia = "https://aguscason.github.io/DesafioClase15/js/tendencia.json" 
+  let prjs = [] 
+  let contenidoHTML = "" ;
 
 
 
 
 
-
-const retornoContenidoPelis = () => {
+const retornoContenidoPelis = (pelicula) => {
+  const {id, titulo, poster} = pelicula ;
   return `<div class="peliculas">
-            <img src="vid/continuarViendo/1.png" alt="">
+            <img id="${pelicula.id}" src="${pelicula.poster}" alt="${pelicula.titulo}" title="${pelicula.titulo}">
           </div>`
 }
 
 
 
-function pedirDatosAlSRV() {
-  fetch(URL)
+const cargarContenido = async () => {
+   await fetch(Drama)
         .then((response) => response.json())
-        .then((data) => console.table(data))
-        .catch((error) => console.log("error", error))
+        .then((data) => {
+          prjs = data ;
+          prjs.forEach(pelicula => {
+            contenidoHTML += retornoContenidoPelis(pelicula) ;
+          });
+          pelisDrama.innerHTML = contenidoHTML ;
+        })
 }
+
+const cargarContenidoRecomendado = async () => {
+  await fetch(Recomendado)
+       .then((response) => response.json())
+       .then((data) => {
+         prjs = data ;
+         prjs.forEach(pelicula => {
+           contenidoHTML += retornoContenidoPelis(pelicula) ;
+         });
+         pelisRecomendaciones.innerHTML = contenidoHTML ;
+       })
+}
+
+const cargarContenidoComedia = async () => {
+  await fetch(Comedia)
+       .then((response) => response.json())
+       .then((data) => {
+         prjs = data ;
+         prjs.forEach(pelicula => {
+           contenidoHTML += retornoContenidoPelis(pelicula) ;
+         });
+         pelisComedia.innerHTML = contenidoHTML ;
+       })
+}
+
+const cargarContenidoTendencia = async () => {
+  await fetch(Tendencia)
+       .then((response) => response.json())
+       .then((data) => {
+         prjs = data ;
+         prjs.forEach(pelicula => {
+           contenidoHTML += retornoContenidoPelis(pelicula) ;
+         });
+         pelisTendencia.innerHTML = contenidoHTML ;
+       })
+}
+
+
+cargarContenidoTendencia() ;
+cargarContenidoRecomendado () ;
+cargarContenido () ;
+cargarContenidoComedia () ;
+
